0ed235a12ac7ebbb64522fffb4aaff610c700bda
Elysium Dev <elysium.dev@icloud.com>
2024-02-10 16:51:32 +0800

    set up firebase for prod

--- src/components/signup/firebase_config.js
+++ src/components/signup/firebase_config.js
@@ -1,14 +1,15 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyD7JeMGfcw2PjrT3V0CjBEKn97aDfk17iY",
-    authDomain: "eventbrite-test-176211.firebaseapp.com",
-    projectId: "eventbrite-test-176211",
-    storageBucket: "eventbrite-test-176211.appspot.com",
-    messagingSenderId: "1074978140837",
-    appId: "1:1074978140837:web:8fc4d7f94315bc8f9bd34c",
-    measurementId: "G-EPG6G820KW"
+    apiKey: "AIzaSyCCK4QuaT7zcN6ppSnol_FASFUvG-H7ZvM",
+    authDomain: "route-planner-413603.firebaseapp.com",
+    projectId: "route-planner-413603",
+    storageBucket: "route-planner-413603.appspot.com",
+    messagingSenderId: "346891522941",
+    appId: "1:346891522941:web:0a441d216980ce0d1d7214",
+    measurementId: "G-H65M989W4V"
 };
+  
 
 export { firebaseConfig };
  
